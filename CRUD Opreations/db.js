6fb583636061b741ccb8bff7ff3cf6748375d8e1
Lav Chaudhari <141054241+LavChaudhari2003@users.noreply.github.com>
2024-12-22 21:22:13 +0530

    Add files via upload

---
+++ CRUD Opreations/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const dotenv  = require('dotenv');
+
+// load env configuration
+dotenv.config();
+
+
+const connectDB = async () => {
+    try {
+        // const conn = await mongoose.connect('mongodb://localhost:27017/test'); // Simplified URI
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log("MongoDB connected:", conn.connection.host);
+    } catch (err) {
+        console.error("Error connecting to MongoDB:");
+        console.error(err); // Log the full error object for better debugging
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
---
+++ CRUD Opreations/models/userModel.js
@@ -0,0 +1,25 @@
+const {Schema , model} = require('mongoose');
+
+const UserSchema = new Schema({
+    name:{
+        type:String,
+        required:true,
+        maxlength:50
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    weight:{
+        type:Number,
+    },
+    createdAt:{
+        type: Date,
+        default : Date.now
+    },
+});
+
+
+const UserModel = model("test", UserSchema)
+
+module.exports = UserModel;
